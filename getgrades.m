908627f9c9e16bf53bc5a4225fa681eb36b9ad55
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-04 21:14:49 -0600

    Update getgrades.m
    change numbers to be close to Grinnell Numbers

--- getgrades.m
+++ getgrades.m
@@ -1,10 +1,9 @@
 function grades = getgrades(classnum, inflation_level)
     if inflation_level == 0
-        dist = pearsrnd(12,0.67,-1.5,5,1,20*classnum);
+        dist = pearsrnd(11,1,-1.5,5,1,20*classnum);
     end
     syms x
 f(x) = piecewise(0<=x<=1.5,0,1.5<x<5,3,x>5,round(x),3<=x<=4,3,4<=x<=5,5);
 grades = f(dist);
-
 
         
