ecb9ce598aaf8418c1b8475dca112646b602933e
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-07 16:29:25 -0600

    added sensitivity test file

---
+++ sensitivity_test.m
@@ -0,0 +1,17 @@
+function [sensitivity,ave,time]=sensitivity_test(num,type)
+tic
+data=randomdata(1700,400,type);
+ave=0;
+sensitivity=zeros(1,num);
+
+for i=1:num
+    [~,~,~,diff12,~,diff13]=grade_sensitivity(data);
+    [~,col1]=size(diff12);
+    %[~,col2]=size(diff23);
+    [~,col3]=size(diff13);
+    combined=col1+col3;
+    sensitivity(1,i)=combined;
+    ave=ave+combined;
+end
+ave=ave/(num*2);
+time=toc;
