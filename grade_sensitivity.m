958ce7e0713f47d51e61ac7d8378563a1fe8ee03
ChrisCecchi <158380101+ChrisCecchi@users.noreply.github.com>
2024-02-06 18:19:47 -0600

    Update grade_sensitivity.m

--- grade_sensitivity.m
+++ grade_sensitivity.m
@@ -12,22 +12,39 @@ newer_data=data;
 [low1,high1,list1]=Project_1_Main(data); %no change in data
 
 nochange=0;
-
-while nochange==0
-    row=randi(Xrow);
-    col=randi([2,Xcol]);
-    val=data(row,col);
-    if ~isnan(val) && val~=0 && val~=13
-        if val==3
-            new_data(row,col)=0;
-            newer_data(row,col)=5;
-        elseif val==5
-            new_data(row,col)=3;
-            newer_data(row,col)=6;
-        else
-            new_data(row,col)=val-1;
-            newer_data(row,col)=val+1;
-        nochange=1;
+type=3;
+if type==0
+    while nochange==0
+        row=randi(Xrow);
+        col=randi([2,Xcol]);
+        val=data(row,col);
+        if ~isnan(val) && val~=0 && val~=13
+            if val==3
+                new_data(row,col)=0;
+                newer_data(row,col)=5;
+            elseif val==5
+                new_data(row,col)=3;
+                newer_data(row,col)=6;
+            else
+                new_data(row,col)=val-1;
+                newer_data(row,col)=val+1;
+            nochange=1;
+            end
+        end
+    end
+elseif type==1
+    %blah
+elseif type==2
+    %blah
+elseif type==3
+    while nochange==0
+        row=randi(Xrow);
+        col=randi([2,Xcol]);
+        val=data(row,col);
+        if ~isnan(val) && val~=0 && val~=1 && val~=2 && val~=98 && val~=99 && val~=100
+            new_data(row,col)=val-3;
+            newer_data(row,col)=val+3;
+            nochange=1;
         end
     end
 end
