c4411e829279902274881ffd93434f8e6c5f41a1
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 14:36:24 -0600

    Update randomdata.m
    added comments, removed unnecessary if statements.

--- randomdata.m
+++ randomdata.m
@@ -1,32 +1,21 @@
 function data=randomdata(row,col,type)
-rng('shuffle');
-if nargin<1
+rng('shuffle'); %seed randomness
+if nargin<1 %if no inputs set inputs as the following
     row=1700;
     col=400;
     type=0;
 end
-students=[1:row]';
-predata=NaN(row,col-1);
-data=[students, predata];
-
-if type==0 %A+,A,A- etc school
-    possible_scores = [0,3,5:13]';
-elseif type ==1 %Grinnell
-    possible_scores = [0,3,5:12]';
-elseif type ==2 %A,B,C school
-    possible_scores = [0:4]';
-elseif type ==3 %0-100
-    possible_scores = [0:100];
-end
-gg=getgrades(400,3);
+students=[1:row]'; %initialize column vector of students
+predata=NaN(row,col-1); %build matrix full of NaN
+data=[students, predata]; %combine students and predata
+gg=getgrades(400,type); %run getgrades to get random set of weighted data
 for i=1:row
     classnum=randi([1,6]); %random number of classes
-    scorenum = randsample(gg,classnum,true);
+    scorenum = randsample(gg,classnum,true); %get grades in each class
     idx=randperm(col-1)+1; %random permutation of classes
     classes=idx(1,1:classnum); %pick first classnum classes
-    for j=1:classnum
+    for j=1:classnum %put the students grades in each class they took into data matrix
         curclass=classes(j);
         data(i,curclass)=scorenum(j);
     end
 end
-
