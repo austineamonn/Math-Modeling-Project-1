569f1f37a0006b75e8d04d613230fe534084f11c
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-10 20:24:34 -0600

    Update Sensitivity_Testing.m

--- Sensitivity_Testing.m
+++ Sensitivity_Testing.m
@@ -6,14 +6,8 @@ end
 final_matrix=zeros(num,4);
 
 for i=1:num
-    for j=1:4
-        [~,ave0,~]=sensitivity_test(10,j); %A+,A,A- etc school
-        %[~,ave1,time1]=sensitivity_test(100,1); %Grinnell
-        %[~,ave2,time2]=sensitivity_test(100,2); %A,B,C school
-        %[~,ave3,time3]=sensitivity_test(100,3); %0-100
+    for j=1:4 %4 types of grading systems loop through with 1 for each
+        [~,ave0,~]=sensitivity_test(10,j);
         final_matrix(i,j)=ave0;
-    %Time=[time0,time1,time2,time3];
-    %Ave=[ave0,ave1,ave2,ave3];
-    %final_matrix()
     end
 end
