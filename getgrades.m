e647f10a11e38e718238db02555c754bb311763f
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 13:49:14 -0600

    Update getgrades.m
    added comments

--- getgrades.m
+++ getgrades.m
@@ -1,21 +1,24 @@
 function dist = getgrades(num,type)
-if type == 0 % A+,...   
-    dist = round(pearsrnd(11,1,-1.5,5,1,num));
+if type == 0 %type 0 is A+,...   
+    dist = round(pearsrnd(11,1,-1.5,5,1,num)); %weighted distribution of grades
     syms x
-    f(x) = piecewise(0<=x<=1.5,0,1.5<x<5,3,x>5,round(x),3<=x<=4,3,4<=x<=5,5,x>13,13);
+    f(x) = piecewise(0<=x<=1.5,0,1.5<x<5,3,x>5,round(x),3<=x<=4,3,4<=x<=5,5,x>13,13); 
+    %function to turn distribution into acceptable values for grading
+    %system type
     dist = f(dist);
-elseif type == 1 %A,A-,B+,...
-    dist = round(pearsrnd(11,1,-1.5,5,1,num));
+elseif type == 1 %type 1 A,A-,B+,... (Grinell)
+    dist = round(pearsrnd(11,1,-1.5,5,1,num)); %weighted distribution of grades
     syms x
     f(x) = piecewise(0<=x<=1.5,0,1.5<x<5,3,x>5,round(x),3<=x<=4,3,4<=x<=5,5,x>12,12);
+    %function to turn distribution into acceptable values for grading
+    %system type
     dist = f(dist);
-elseif type == 2 %A,B,C,...
-    
-        dist = round(pearsrnd(4,0.5,-1.5,5,1,num));
-        dist(dist>4)=4;
-  
+elseif type == 2 %type 2 is A,B,C,...
+    dist = round(pearsrnd(4,0.5,-1.5,5,1,num)); %weighted distribution of grades
+    dist(dist>4)=4; %function to turn distribution into acceptable values for grading
+    %system type
 else %type 3 is 0-100
-        dist = round(pearsrnd(91,5,-1.5,5,1,num));
-  
-end
-        
+    dist = round(pearsrnd(91,5,-1.5,5,1,num)); %weighted distribution of grades
+    %function to turn distribution into acceptable values for grading
+    %system type
+end        
