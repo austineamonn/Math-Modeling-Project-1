759b1a425b72642cff9df0ad42c1cca6692d5317
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 13:43:14 -0600

    Update class_ave.m

--- class_ave.m
+++ class_ave.m
@@ -4,7 +4,7 @@ sum=0; %initialize sum
 count=0; %initialize count
 for i = 1:row %go through class calculate total number of students and their grades
    k = class(i,1);
-    if ~isnan(k) 
+    if ~isnan(k) %exclude students who are not in the class
         sum=sum+k;
         count=count+1;
     end
