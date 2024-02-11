ea6047471653a17493777c71224c11511e0a4f01
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-10 20:00:01 -0600

    Update testofSensitivity_Testing.m

--- testofSensitivity_Testing.m
+++ testofSensitivity_Testing.m
@@ -1,7 +1,12 @@
-function [stand_dev,themean]=testofSensitivity_Testing(matrix)
+function [stand_dev,themean,Anova,time]=testofSensitivity_Testing(matrix)
+tic
 if nargin<1
     matrix=Sensitivity_Testing(101);
 end
 
-stand_dev=std(matrix,0,2);
-themean=mean(matrix,2);
+stand_dev=std(matrix,0,1); %column vector of std
+themean=mean(matrix,1); %column vector of mean
+
+Anova=anova(matrix);
+
+time=toc;
