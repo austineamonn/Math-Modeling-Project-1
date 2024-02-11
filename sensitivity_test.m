27aff09452cf42f189799fd9c8670334596b2f36
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-10 20:12:53 -0600

    Update sensitivity_test.m

--- sensitivity_test.m
+++ sensitivity_test.m
@@ -5,7 +5,7 @@ ave=0;
 sensitivity=zeros(1,num);
 
 for i=1:num
-    [~,~,~,diff12,~,diff13]=grade_sensitivity(data);
+    [~,~,~,diff12,diff13]=grade_sensitivity(data);
     [~,col1]=size(diff12);
     [~,col3]=size(diff13);
     combined=col1+col3;
