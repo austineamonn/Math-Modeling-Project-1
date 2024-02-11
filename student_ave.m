9bdc4122e62ef8c9162d11fd7736cb45026782d9
ChrisCecchi <158380101+ChrisCecchi@users.noreply.github.com>
2024-02-11 14:41:36 -0600

    Update student_ave.m

--- student_ave.m
+++ student_ave.m
@@ -1,10 +1,10 @@
-function stud_ave=student_ave(class,ave)
+function stud_ave=student_ave(class,ave) 
 [row,~]=size(class);
 stud_ave=class;
-for i = 1:row
-   k = class(i,1);
-   j= stud_ave(i,1);
-    if k>-1
+for i = 1:row 
+   k = class(i,1); % grade in class 
+   j= stud_ave(i,1); % student's average score in class
+    if k>-1 % active only if student has real grade in class
         stud_ave(i,1)=j-ave;
     end
 end
