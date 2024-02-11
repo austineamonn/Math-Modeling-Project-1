5d99d194df3258f4a5ad4e9b57acd244ec205f3b
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 14:52:52 -0600

    Update testofSensitivity_Testing.m
    added comments

--- testofSensitivity_Testing.m
+++ testofSensitivity_Testing.m
@@ -1,12 +1,12 @@
 function [stand_dev,themean,Anova,time]=testofSensitivity_Testing(matrix)
 tic
-if nargin<1
+if nargin<1 %if there is matrix input call Sensitivity_Testing to make one
     matrix=Sensitivity_Testing(101);
 end
 
-stand_dev=std(matrix,0,1); %column vector of std
-themean=mean(matrix,1); %column vector of mean
+stand_dev=std(matrix,0,1); %calculate standard deviation by each column
+themean=mean(matrix,1); %calculate mean  by each column
 
-Anova=anova(matrix);
+Anova=anova(matrix); %calculate ANOVA of matrix
 
-time=toc;
+time=toc; %get total time to run program.
