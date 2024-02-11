e994f3a7fa311d79e3a247d422f3c55938373ca4
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 14:47:32 -0600

    Update student_ave.m
    added more comments

--- student_ave.m
+++ student_ave.m
@@ -1,10 +1,10 @@
 function stud_ave=student_ave(class,ave) 
-[row,~]=size(class);
-stud_ave=class;
+[row,~]=size(class); %get dimensions of class
+stud_ave=class; %intialize stud_ave
 for i = 1:row 
    k = class(i,1); % grade in class 
    j= stud_ave(i,1); % student's average score in class
     if ~isnan(k) % exclude students who are not in the class
-        stud_ave(i,1)=j-ave;
+        stud_ave(i,1)=j-ave; %replace student's grade with difference from average grade
     end
 end
