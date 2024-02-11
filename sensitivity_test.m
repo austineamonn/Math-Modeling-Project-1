8e9e60cec64d1f0b3b3ce113bfd8f00b13fe9cb6
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 14:42:28 -0600

    Update sensitivity_test.m
    added comments

--- sensitivity_test.m
+++ sensitivity_test.m
@@ -1,16 +1,14 @@
-function [sensitivity,ave,time]=sensitivity_test(num,type)
-tic
-data=randomdata(1700,400,type);
-ave=0;
-sensitivity=zeros(1,num);
+function [sensitivity,ave]=sensitivity_test(num,type)
+data=randomdata(1700,400,type); %get random data
+ave=0; %initialize ave at zero
+sensitivity=zeros(1,num); %initialize sensitivity
 
-for i=1:num
+for i=1:num %run grade sensitivity num times
     [~,~,~,diff12,diff13]=grade_sensitivity(data,type);
-    [~,col1]=size(diff12);
+    [~,col1]=size(diff12); %get size of difference matrices
     [~,col3]=size(diff13);
-    combined=col1+col3;
-    sensitivity(1,i)=combined;
+    combined=col1+col3; %combine sizes
+    sensitivity(1,i)=combined; %put combined into the sensitivity vector
     ave=ave+combined;
 end
-ave=ave/(num*2);
-time=toc;
+ave=ave/(num*2); %solve for average of difference matrices
