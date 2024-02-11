08460ca740ff5e0509c4f9fa1ae0ce09a4b54c08
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 15:26:43 -0600

    Update testofSensitivity_Testing.m
    got rid of time

--- testofSensitivity_Testing.m
+++ testofSensitivity_Testing.m
@@ -1,5 +1,5 @@
 function [stand_dev,themean,Anova,time]=testofSensitivity_Testing(matrix)
-tic
+
 if nargin<1 %if there is matrix input call Sensitivity_Testing to make one
     matrix=Sensitivity_Testing(101);
 end
@@ -8,5 +8,3 @@ stand_dev=std(matrix,0,1); %calculate standard deviation by each column
 themean=mean(matrix,1); %calculate mean  by each column
 
 Anova=anova(matrix); %calculate ANOVA of matrix
-
-time=toc; %get total time to run program.
