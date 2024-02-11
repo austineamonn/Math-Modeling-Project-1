4c6f149ed23888d7398558e75dbf2270e509245f
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-11 14:43:57 -0600

    Update student_ave.m
    slight change

--- student_ave.m
+++ student_ave.m
@@ -4,7 +4,7 @@ stud_ave=class;
 for i = 1:row 
    k = class(i,1); % grade in class 
    j= stud_ave(i,1); % student's average score in class
-    if k>-1 % active only if student has real grade in class
+    if ~isnan(k) % exclude students who are not in the class
         stud_ave(i,1)=j-ave;
     end
 end
