af93e536f544f336dda6cdb630e6c8823ddfb5e9
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-10 20:36:50 -0600

    Update Sensitivity_Testing.m

--- Sensitivity_Testing.m
+++ Sensitivity_Testing.m
@@ -7,7 +7,7 @@ final_matrix=zeros(num,4);
 
 for i=1:num
     for j=1:4 %4 types of grading systems loop through with 1 for each
-        [~,ave0,~]=sensitivity_test(10,j);
+        [~,ave0,~]=sensitivity_test(10,j-1);
         final_matrix(i,j)=ave0;
     end
 end
