5029b0145b5bde6d2b583fc8e3f3b861bc551e58
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-10 20:13:37 -0600

    Update grade_sensitivity.m

--- grade_sensitivity.m
+++ grade_sensitivity.m
@@ -1,4 +1,4 @@
-function [row,col,val,diff12,diff23,diff13]=grade_sensitivity(data)
+function [row,col,val,diff12,diff13]=grade_sensitivity(data)
 rng('shuffle')
 
 if nargin<1
@@ -9,11 +9,11 @@ end
 new_data=data;
 newer_data=data;
 
-[low1,high1,list1]=Project_1_Main(data); %no change in data
+[low1,~,~]=Project_1_Main(data); %no change in data
 
 nochange=0;
 type=3;
-if type==0
+if type==0 % A+,...
     while nochange==0
         row=randi(Xrow);
         col=randi([2,Xcol]);
@@ -32,11 +32,37 @@ if type==0
             end
         end
     end
-elseif type==1
-    %blah
-elseif type==2
-    %blah
-elseif type==3
+elseif type==1 %Grinnell
+    while nochange==0
+        row=randi(Xrow);
+        col=randi([2,Xcol]);
+        val=data(row,col);
+        if ~isnan(val) && val~=0 && val~=12
+            if val==3
+                new_data(row,col)=0;
+                newer_data(row,col)=5;
+            elseif val==5
+                new_data(row,col)=3;
+                newer_data(row,col)=6;
+            else
+                new_data(row,col)=val-1;
+                newer_data(row,col)=val+1;
+            nochange=1;
+            end
+        end
+    end
+elseif type==2 %A,B,C,...
+    while nochange==0
+        row=randi(Xrow);
+        col=randi([2,Xcol]);
+        val=data(row,col);
+        if ~isnan(val) && val~=0 && val~=4
+            new_data(row,col)=val-1;
+            newer_data(row,col)=val+1;
+            nochange=1;
+        end
+    end
+elseif type==3 %type 3 is 0-100
     while nochange==0
         row=randi(Xrow);
         col=randi([2,Xcol]);
@@ -49,10 +75,9 @@ elseif type==3
     end
 end
 
-[low2,high2,list2]=Project_1_Main(new_data); %change to go down 1
+[low2,~,~]=Project_1_Main(new_data); %change to go down 1
 
-[low3,high3,list3]=Project_1_Main(newer_data); %change to go up 1
+[low3,~,~]=Project_1_Main(newer_data); %change to go up 1
 
 diff12=setdiff(low1,low2); %no highs
-diff23=setdiff(low2,low3);
 diff13=setdiff(low1,low3);
