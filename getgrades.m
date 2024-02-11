95932517dbb4a8dfcf7756bf6b5c82f1b1f63cb3
ChrisCecchi <158380101+ChrisCecchi@users.noreply.github.com>
2024-02-10 20:13:31 -0600

    Update getgrades.m

--- getgrades.m
+++ getgrades.m
@@ -1,20 +1,21 @@
-function dist = getgrades(classnum, inflation_level, type)
+function dist = getgrades(num, type)
 if type == 0 % A+,...   
-    if inflation_level == 0
-        dist = pearsrnd(11,1,-1.5,5,1,20*classnum);
-    end
+    dist = round(pearsrnd(11,1,-1.5,5,1,num));
     syms x
-    f(x) = piecewise(0<=x<=1.5,0,1.5<x<5,3,x>5,round(x),3<=x<=4,3,4<=x<=5,5);
+    f(x) = piecewise(0<=x<=1.5,0,1.5<x<5,3,x>5,round(x),3<=x<=4,3,4<=x<=5,5,x>13,13);
+    dist = f(dist);
+elseif type == 1 %A,A-,B+,...
+    dist = round(pearsrnd(11,1,-1.5,5,1,num));
+    syms x
+    f(x) = piecewise(0<=x<=1.5,0,1.5<x<5,3,x>5,round(x),3<=x<=4,3,4<=x<=5,5,x>12,12);
     dist = f(dist);
 elseif type == 2 %A,B,C,...
-    if inflation_level == 0
-        dist = round(pearsrnd(4,0.5,-1.5,5,1,20*classnum));
+    
+        dist = round(pearsrnd(4,0.5,-1.5,5,1,num));
         dist(dist>4)=4;
-    end
+  
 else %type 3 is 0-100
-    if inflation_level == 0
-        dist = round(pearsrnd(91,5,-1.5,5,1,20*classnum));
-    end
+        dist = round(pearsrnd(91,5,-1.5,5,1,num));
+  
 end
-
         
